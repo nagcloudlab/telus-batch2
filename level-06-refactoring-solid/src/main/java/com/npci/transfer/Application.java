e045468e65bbe4b0a7338fb56cb83ed1a2efbde8
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2025-12-21 15:49:17 +0530

    level-08

---
+++ level-06-refactoring-solid/src/main/java/com/npci/transfer/Application.java
@@ -0,0 +1,9 @@
+package com.npci.transfer;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
